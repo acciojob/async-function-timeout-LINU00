618af8511dce955cdc2081e7b4525f14999ebd92
Aditya Singh <103933450+LINU00@users.noreply.github.com>
2025-10-03 03:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+async function showMessage(message, delay) {
+    await new Promise(resolve => setTimeout(resolve, delay));
+    document.getElementById('output').innerText = message;
+}
+
+document.getElementById('btn').addEventListener('click', () => {
+    const text = document.getElementById('text').value;
+    const delay = Number(document.getElementById('delay').value);
+    showMessage(text, delay);
+});
